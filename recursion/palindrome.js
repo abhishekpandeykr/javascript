88c2a23a30956972828a449b46f1dd85011a785f
abhishekpandeykr <abhishekpandey.kr@gmail.com>
2019-05-28 22:38:52 +0530

    palindrome through string recursion

---
+++ recursion/palindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(isPalinStrng) {
+    if(isPalinStrng.length === 1) {
+        return true;
+    } else {
+        const strLen = isPalinStrng.length;
+        const [firstChar, lastChar] = [isPalinStrng.charAt(0), isPalinStrng.charAt(strLen-1)];
+        if(firstChar === lastChar) {
+            const modifiedString = isPalinStrng.substr(1, strLen-2);
+            return isPalindrome(modifiedString);
+        } else {
+            return false;
+        }
+    }
+}
+
+console.log(isPalindrome('abhishek'));
+console.log(isPalindrome('12321'));
